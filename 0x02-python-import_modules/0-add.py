ea6d6e348516f135c8ed8d8a2b62b229d4e77b25
danieljonesobot <daniel.jonesobot@gmail.com>
2023-06-08 10:36:02 -0700

    fixed import fucntion call

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import  add_0
 
 a = 1
 b = 2
 
-print("{} + {} = {} ".format(a, b, add_0.add(a, b)))
-
+if __name__ == "__main__":
+    from add_0 import add
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
