cb9fc966c3fc6544cf440c423f56f9eafc835a71
danieljonesobot <daniel.jonesobot@gmail.com>
2023-06-29 09:44:43 -0700

    first commit task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+The function  returns the sum of two integers.
+"""
+def add_integer(a, b=98):
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
