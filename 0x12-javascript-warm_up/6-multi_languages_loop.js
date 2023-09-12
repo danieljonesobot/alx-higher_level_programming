fd2bda316637570110038c9be29f5e1c745f906c
danieljonesobot <daniel.jonesobot@gmail.com>
2023-09-12 20:20:06 +0000

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//script that prints 3 lines using an array of string and a loop
+
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myString.length; i++){
+	console.log(myString[i]);
+}
