bbce14788e0f48ca6d806ba77b8ef43b8ed96f12
danieljonesobot <daniel.jonesobot@gmail.com>
2023-09-12 20:22:33 +0100

    update to task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 //script that prints a message depending of the number of arguments passed
 
-if (process.argv.length === 2) {
+const { argv } = require('process');
+if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]); }
+
+
+/*if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
-}
+}*/
